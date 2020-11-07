7862753a5c563d06e434374fb34e5aa7f145e58a
William Reames <william.reames.19@cnu.edu>
2020-11-07 15:46:10 -0500

    Updated to work for Spring 2021

--- src/course_selector.py
+++ src/course_selector.py
@@ -1,42 +1,47 @@
+
 import os
-from selenium import webdriver
-from datetime import datetime
-import time
 import pause
+import time
+from datetime import datetime
+from selenium import webdriver
+from getpass import getpass
 
 # Enter your preferred classes here:
-classes = ['8116']
+classes = ['8367', '8374', '8880', '8368', '8657', '8373', '8661']
+
+# Note that the registrar won't let you register for >18 credits and if you try to the program will mess up
 # Also make sure to change the registration time!!! (line 40)
+
+# Gathers user input
+id = input('Enter your student id: ')
+password = getpass()  # Needs to be run on terminal for this to work!
+with open(os.path.join('data', 'registration_key.txt'), 'rt') as file:  # Reads in input data from file
+    keycode = file.readline().strip()
+
+print('Connecting to CNU...')
 
 # Makes a connection to the internet through a Chrome driver
 driver = webdriver.Chrome(os.path.join(os.getcwd(), 'data', 'chromedriver'))
 driver.get('https://banweb.cnu.edu/banweb/twbkwbis.P_WWWLogin')
 
 try:
-    id = ''
-    password = ''
-    keycode = ''
-    with open(os.path.join('data', 'keys.txt'), 'rt') as file:  # Reads in input data from file
-        id = file.readline().strip()
-        password = file.readline().strip()
-        keycode = file.readline().strip()
-
     # Logs into CNU Live and navigates to class registration
     driver.find_element_by_id('UserID').send_keys(id)
     driver.find_element_by_name('PIN').send_keys(password + '\n')  # '\n' used in place of 'enter'
+    time.sleep(0.1)  # Pauses so that way it has time to load the next page
     driver.find_element_by_name('StuWeb-MainMenuLink').click()
     driver.find_element_by_link_text('Registration').click()
     driver.find_element_by_link_text('Add/Drop Classes').click()
 
     # Chooses Semester
-    driver.find_element_by_xpath("//select[@name='term_in']/option[text()='Fall Semester 2020']").click()
+    driver.find_element_by_xpath("//select[@name='term_in']/option[text()='Spring Semester 2021']").click()
     driver.find_element_by_name('term_in').submit()
 
     # Waits until the exact opening time to enter the pin
     # Best to enter the exact moment that classes open rather than half a second early
     # Year, Month, Day, Hour (military-time), Minute, Second, Millisecond
     print(datetime.now())
-    pause.until(datetime(2020, 5, 29, 8, 0, 0, 0))
+    pause.until(datetime(2020, 11, 11, 7, 0, 0, 0))
     print(datetime.now())
 
     # Enters the Alternate PIN
