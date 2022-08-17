7efde1e67ee74d32d58f849fef231ed58ddf2dc1
William Reames <william.reames.19@cnu.edu>
2022-08-17 11:02:28 -0400

    Added functionality for an overnight run; updated code

--- course_selector.py
+++ course_selector.py
@@ -1,7 +1,7 @@
 import os
 import pause
 import time
-from datetime import datetime
+import datetime
 from selenium import webdriver
 from getpass import getpass
 
@@ -21,7 +21,41 @@ with open(os.path.join('data', 'registration_info.txt'), 'rt') as file:
     semester = file.readline().split(':', 1)[1].strip()
     date_str = file.readline().split(':', 2)[2].strip()
 
-registration_date = datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+registration_date = datetime.datetime.strptime(date_str, '%Y-%m-%d %H:%M')
+
+print('Testing Username and Password...')
+
+# Run driver without waiting
+# Wait until close to login
+# Run and wait until correct time
+
+driverConfirmation = webdriver.Chrome(os.path.join(os.getcwd(), 'data', 'chromedriver'))
+driverConfirmation.get('https://banweb.cnu.edu/banweb/twbkwbis.P_WWWLogin')
+# Logs into CNU Live and navigates to class registration
+driverConfirmation.find_element_by_id('UserID').send_keys(student_id)
+driverConfirmation.find_element_by_name('PIN').send_keys(password + '\n')  # '\n' used in place of 'enter'
+
+# Pauses so that way it has time to load the next page
+time.sleep(0.1)
+driverConfirmation.find_element_by_name('StuWeb-MainMenuLink').click()
+driverConfirmation.find_element_by_link_text('Registration').click()
+driverConfirmation.find_element_by_link_text('Add/Drop Classes').click()
+
+# Chooses Semester
+driverConfirmation.find_element_by_xpath("//select[@name='term_in']/option[text()='{}']".format(semester)).click()
+driverConfirmation.find_element_by_name('term_in').submit()
+
+# Enters the pin
+driverConfirmation.find_element_by_name('pin').send_keys(keycode)
+
+# Begins registration
+driverConfirmation.find_element_by_name('pin').submit()
+pause.sleep(5)
+driverConfirmation.quit()
+
+window_start_time = registration_date - datetime.timedelta(minutes=5)
+print('Waiting until {} to start...'.format(window_start_time))
+pause.until(window_start_time)
 
 print('Connecting to CNU...')
 
@@ -49,9 +83,10 @@ try:
     # Waits until the exact opening time to enter the pin
     # Best to enter the exact moment that classes open rather than half a second early
     # Year, Month, Day, Hour (military-time), Minute, Second, Millisecond
-    print('Current time:  {}'.format(datetime.now()))
-    print('Waiting until: {}'.format(registration_date))
-    pause.until(registration_date)
+    altered_reg_date = registration_date + datetime.timedelta(milliseconds=2500)
+    print('Current time:  {}'.format(datetime.datetime.now()))
+    print('Waiting until: {}'.format(altered_reg_date))
+    pause.until(altered_reg_date)
     # TODO: Important: If the time you log in when testing is earlier than it should be,
     # TODO: add a slight delay to the program so it can register at the correct time
     # TODO: (don't want to be half a second early)
@@ -69,5 +104,5 @@ except Exception as e:
     print(e)
 finally:
     # Keeps the tab open for another five minutes
-    time.sleep(300)
+    time.sleep(3000)
     driver.quit()
